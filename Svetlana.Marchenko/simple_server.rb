718666802c68ad70097bee84f60456fefc135d9b
Svetlana <lana@lana-note.(none)>
2011-06-21 10:53:06 +0400

    Svetlana Marchenko: server bugs were fixed

--- Svetlana.Marchenko/simple_server.rb
+++ Svetlana.Marchenko/simple_server.rb
@@ -1,8 +1,23 @@
+=begin
+  Author: Svetlana Marchenko
+  This is simple web server that can serve requests for some content types (txt, html, css, ruby files, images, css etc). 
+  It has logging support.
+  Multi-thread mechanism.
+  http status codes (200, 400, 404, 500) support.
+  No https.
+  
+=end
+
+$:.unshift(File.expand_path("./"))
+
 require 'socket'
+require 'generator'
+
 
 def writeIntoLog(message)
   logStr =  "\n\n\n\n\n\n#{message}"
   $log.puts logStr if $log != nil
+  $stderr.puts logStr if $log != nil
 end
 
 class HttpServer
@@ -10,102 +25,84 @@ class HttpServer
     @session = session
     @request = request
     @basePath = basePath
-		@status = {
-			200=>"OK",
-			304=>"Modified",
-			400=>"Bad_Request",
-			404=>"Not_Found",
-			500=>"Internal_Server_Error"
-		}
+    @status = {
+      200=>"OK",
+      304=>"Modified",
+      400=>"Bad_Request",
+      404=>"Not_Found",
+      500=>"Internal_Server_Error"
+      }
+    @contentTypes = {
+      ".html"=>"text/html",
+      ".htm" =>"text/html",
+      ".txt" =>"text/plain",
+      ".rb"  =>"text/plain",
+      ".jpeg"=>"image/jpeg",
+      ".jpg" =>"image/jpeg",
+      ".gif" =>"image/gif",
+      ".bmp" =>"image/bmp",
+      ".png" =>"image/png",
+      ".xml" =>"text/xml",
+      ".mpg" =>"video/mpeg",
+      ".zip" =>"application/zip",
+      ".pdf" =>"application/pdf",
+      ".mp3" =>"audio/mpeg"
+      }
+    @contentTypes.default = "unknown/unknown"
   end
 
   def getFullPath()
     fileName = nil
     if @request =~ /GET .* HTTP.*/
       fileName = @request.gsub(/GET /, '').gsub(/ HTTP.*/, '')
-		end
+    end
     fileName = fileName.strip
-		unless fileName == nil
+    unless fileName == nil
       fileName = @basePath + fileName
-			fileName = File.expand_path(fileName, @defaultPath)
+      fileName = File.expand_path(fileName, @defaultPath)
     end
     fileName << "/index.html" if  File.directory?(fileName)
-		return fileName
+    return fileName
   end
 
   def serve()
-		@fullPath = getFullPath()
-		begin
-			contentType = getContentType(@fullPath)
-			print "#{contentType}"
-			if (contentType != "error")
-	      if File.exist?(@fullPath) and File.file?(@fullPath)
-					if @fullPath.index(@basePath) == 0 
-							okMessage = "HTTP/1.1 200/OK\r\nServer: Simple Http Server On Ruby\r\nContent-type: #{contentType}\r\n\r\n"
-							writeIntoLog(okMessage)
-				  		@session.print okMessage
-							ext = File.extname(@fullPath)
-							if ext == ".rb"
-								@session.print `ruby #{@fullPath}` 
-							else
-								printFileContent(@fullPath)
-							end
-					else #endif fullPath starts with basePath
-						handleError(400, @status[400])
-					end
-  	    else #endif File.exist
-					handleError(404, @status[404])
-  	    end
-			else #endif error content type
-				handleError(500, @status[500])
-			end  
+    @fullPath = getFullPath()
+    begin
+      contentType = getContentType(@fullPath)
+      if File.exist?(@fullPath) and File.file?(@fullPath)
+	if @fullPath.index(@basePath) == 0 
+	  header = generateHeader(200, @status[200], contentType)
+	  print header
+	  writeIntoLog(header)
+	  @session.print header
+	  ext = File.extname(@fullPath)
+	  if ext == ".rb"
+	    @session.print `ruby #{@fullPath}`
+	  else
+	    getFileContent(@fullPath)
+	  end
+	else #endif fullPath starts with basePath
+	  @session.print generateHeader(400, @status[400], @contentTypes[".html"])
+	  @session.print generateErrorPage(400, @status[400])
+	end
+      else #endif File.exist
+	@session.print generateHeader(404, @status[404], @contentTypes[".html"])
+	@session.print generateErrorPage(404, @status[404])
+      end
     ensure
       @session.close
     end
   end
 
-	def printFileContent(path)
-		src = File.open(path, "rb")
-   	while (not src.eof?)
-     	buffer = src.read(256)
-     	@session.write(buffer)
-   	end
-   	src.close
-   	src = nil
-	end
-
-	def handleError(status, message)
-		command = "ruby error.rb #{status} #{message}"
-		@session.print `#{command}`
-	end
+  def getFileContent(path)
+    contentFile = IO.read(path)
+    @session.print contentFile
+  end
+  
 
   def getContentType(path)
-
     ext = File.extname(path)
-		case ext
-			when ".html"
-				return "text/html"
-			when ".htm"
-				return "text/html"
-			when ".txt"
-				return "text/plain"
-			when ".css"
-				return "text/css"
-			when ".jpeg"
-				return "image/jpeg"
-			when ".jpg"
-				return "image/jpeg"
-			when ".gif"
-				return "image/gif"
-			when ".bmp"
-				return "image/bmp"
-			when ".xml"
-				return "text/xml"
-			when ".rb"
-				return "text/plain"
-			else
-				return "error"
-		end
+    @contentTypes[ext]
   end
 end
 
@@ -116,12 +113,13 @@ end
 if (ARGV.length > 0)
 	basePath = ARGV[0]
 else
-	basePath = "/home/lana/Study/unix"
+	basePath = File.expand_path("./")
 end
 
 server = TCPServer.new('127.0.0.1', 9090)
 logfile = basePath + "/log.txt"
 $log = File.open(logfile, "a")
+$log.sync = true
 
 loop do
   session = server.accept
