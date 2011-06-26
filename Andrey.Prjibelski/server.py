cc25651caf83979e7687610e9a079f71882547fd
Andrey <andrewprzh@gmail.com>
2011-06-27 03:18:03 +0400

    Logger added

--- Andrey.Prjibelski/server.py
+++ Andrey.Prjibelski/server.py
@@ -1,68 +1,102 @@
 #!/usr/bin/python -O
 
+# This is very simple http server written on python.
+# One can display basic content such as images, html pages, e.t.c.
+# Also it you can run python script on it and see result as web page.
+#
+# To start server just run this file as "./server.py"
+# If file appears to be non-executabe use "chmod +x server.py"
+# (you need python to be installed on your computer)
+# 
+# Server will run on standart 8080 port.
+# Type "http://localhost:8080/<filename>" in your browser to try it.
+#
+# (C) Andrey Prjibelski
+
+
 import socket
 import sys
+import os
+import datetime
+
+#Log file
+logFile = open("log.txt","a")
+
+#Log message
+def log(msg):
+	logFile.write(str(datetime.datetime.now())+" "+msg+"\n")
+
+print "Server started"
+log("Server started")
 
 #HTTP request methods
 HTTPMethods = ["OPTIONS", "GET", "HEAD", "PUT", "POST", "DELETE", "TRACE", "CONNECT"]
 
-#HTTP headers
-HTTPHeaderOK = "HTTP/1.1 200 OK\n\n"
-HTTPHeaderBadRequest = "HTTP/1.1 400 Bad Request\n\n"
-HTTPHeaderNotFound = "HTTP/1.1 404 Not Found\n\n"
-HTTPHeaderMethodNotAllowed = "HTTP/1.1 405 Method Not Allowed\n\n"
-HTTPHeaderUnsupported = "HTTP/1.1 415 Unsupported Media Type\n\n"
-HTTPHeaderInternalError = "HTTP/1.1 500 Internal Server Error\n\n"
+#Supported media type
+contentType = {
+       ".html": "text/html", ".htm": "text/html", ".txt": "text/plain",
+       ".jpeg": "image/jpeg", ".jpg": "image/jpeg", ".gif": "image/gif", ".png": "image/png",
+       ".mp3": "audio/mpeg", ".mpeg": "video/mpeg", ".avi": "video/mpeg",
+       ".zip": "application/zip", ".gz": "application/x-gzip", ".pdf": "application/pdf",
+       }
+
+#Content header
+def ContentHeader(content):
+	return " Content-type: "+content+"\n\n"
+
+#HTTP Status Codes
+HTTPStatus = {
+	200: "OK", 304: "Not Modified", 400: "Bad Request", 404: "Not Found", 405: "Method Not Allowed", 
+	415: "Unsupported Media Type", 500: "Internal Server Error", 
+	}
 
 
-#Host and port number
-host = ''
-port = 8080
+#Status text
+def StatusText(code):
+	return str(code)+" "+HTTPStatus[code]
+		
 
-#Creating sockets
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#Status header
+def StatusHeader(code):
+	return "HTTP/1.1 "+StatusText(code)
 
-#Error pages
+#Status pages
+def StatusPage(code, msg):
+	log(StatusText(code)+" "+msg)
+	title = StatusText(code)
+	return StatusHeader(code)+"\n\n<html><head><title>"+title+"</title></head><body> \
+	<h2>"+title+"</h2>"+msg+" \
+	</body></html>"
+
+#200
+def HTTP200OK():
+	log(StatusText(200))
+	return StatusHeader(200)
+
+#304
+def HTTP304NotModified():
+	log(StatusText(304))
+	return StatusHeader(304)	
+
 #400
 def HTTP400BadRequest(query):
-	return HTTPHeaderBadRequest+"  \
-	<html><head><title>400 Bad Request</title></head><body> \
-	<h2>400 Bad Request</h2> \
-	Request "+query+" could not be understood by the server.\
-	<br></body></html>"
+	return StatusPage(400, "Request "+query+" could not be understood by the server.")
 
 #404
 def HTTP404NotFound(fileName):
-	return HTTPHeaderNotFound+" \
-	<html><head><title>404 Not Found</title></head> \
-	<body><h2>404 Not Found</h2> \
-	"+fileName+" cannot be found.<br>\
-	</body></html>"
+	return StatusPage(404, fileName+" cannot be found.")
 
 #405
 def HTTP405MethodNotAllowed(method):
-	return HTTPHeaderMethodNotAllowed+" \
-	<html><head><title>405 Method Not Allowed</title></head> \
-	<body><h2>405 Method Not Allowed</h2> \
-	Used method "+method+" is not allowed. Available methods: <br> \
-	GET <br></body></html>"
+	return StatusPage(405, "Used method "+method+" is not allowed. Available methods: <br> GET")
 
 #415
 def HTTP415UnsupportedMediaType(ext):
-	return HTTPHeaderUnsupported+" \
-	<html><head><title>415 Unsupported Media Type</title></head> \
-	<body><h2>415 Unsupported Media Type</h2> \
-	Requested file seems to have unsupported extention "+ext+". <br> \
-	</body></html>"
+	return StatusPage(415, "Requested file seems to have unsupported extention "+ext+".")
 
 #500
 def HTTP500InternalError():
-	return HTTPHeaderInternalError+" \
-	<html><head><title>500 Internal Server Error</title></head> \
-	<body><h2>500 Internal Server Error</h2> \
-	Internal server error occured. <br> \
-	</body></html>"
+	return ErrorPage(500, "Internal server error occured.")
 
 
 #Query ecexutor
@@ -86,51 +120,54 @@ def openFile(fileName):
 	#Default is index.html
 	if fileName == "/":
 		fileName = "/index.html"
-	#Get file extention
-	try:
-		ext = fileName.rsplit('.', 1)[1]
-	except IndexError:
-		return HTTP415UnsupportedMediaType("")
 
-	#If file in not python script, but format is supported
-	if ext in ["txt", "htm", "html"]:
+	#Get file extention
+	ext = os.path.splitext(fileName)[1]
+
+	if ext == ".py":
+		#Python sctipt
 		try:
-			inFile = open("."+fileName, "r")
-			#Display text file prorely
-			if ext == "txt":
-				data = "<br>".join(inFile.readlines())
-				return HTTPHeaderOK+"<html><head><title>"+fileName+"</title></head> \
-				<body>"+data+"</body></html>"
-			#Html displayed as is
-			else:
-				data = inFile.read()
-				return HTTPHeaderOK+data
+			#Redirecting output
+			bufferFile = open("data", "w")
+			sys.stdout = bufferFile
+			sys.stderr = bufferFile
+			#Executing
+			execfile("."+fileName)
+			bufferFile.close()
+			#Reading data properly
+			bufferFile = open("data", "r")
+			data = "<br>".join(bufferFile.readlines())
+			return HTTP200OK()+ContentHeader(contentType.get(".html"))+"<html><head><title>"+fileName+"</title></head> \
+			<body>"+data+"</body></html>"
 		except IOError:
 			return HTTP404NotFound(fileName)
+		finally:
+			#Returning output to its standart values
+			sys.stdout = sys.__stdout__
+			sys.stderr = sys.__stderr__
 	else:
-		#Python sctipt
-		if ext == "py":
-			try:
-				#Redirecting output
-				bufferFile = open("data", "w")
-				sys.stdout = bufferFile
-				sys.stderr = bufferFile
-				#Executing
-				execfile("."+fileName)
-				bufferFile.close()
-				#Reading data properly
-				bufferFile = open("data", "r")
-				data = "<br>".join(bufferFile.readlines())
-				return HTTPHeaderOK+"<html><head><title>"+fileName+"</title></head> \
-				<body>"+data+"</body></html>"
-			except IOError:
-				return HTTP404NotFound(fileName)
-			finally:
-				#Returning output to its standart values
-				sys.stdout = sys.__stdout__
-				sys.stderr = sys.__stderr__
+		#Get content type
+		if contentType.get(ext) != None:
+			content = contentType[ext]
 		else:
+			#Unsupported media type
 			return HTTP415UnsupportedMediaType(ext)
+
+		#If file in not python script, but format is supported
+		try:
+			inFile = open("."+fileName, "r")
+			return HTTP200OK()+ContentHeader(content)+inFile.read()
+		except IOError:
+			return HTTP404NotFound(fileName)
+		
+
+#Host and port number
+host = ''
+port = 8080
+
+#Creating sockets
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 		
 #Binding socket
@@ -138,14 +175,22 @@ serverSocket.bind((host, port))
 serverSocket.listen(1)
 
 #Main server loop
-while 1: 
-	#Recieving request
-	cSocket, cAddress = serverSocket.accept() 
-	dataFile = cSocket.makefile('rw', 0) 
-	query = dataFile.readline().strip()
-	#Executing query
-	result = executeQuery(query)
-	#Sending data to client
-	dataFile.write(result)
-	dataFile.close() 
-	cSocket.close()
+try:
+	while 1: 
+		#Recieving request
+		cSocket, cAddress = serverSocket.accept()
+		dataFile = cSocket.makefile('rw', 0) 
+		query = dataFile.readline().strip()
+		log("Request "+query+" from "+str(cAddress))
+		#Executing query
+		result = executeQuery(query)
+		#Sending data to client
+		dataFile.write(result)
+		dataFile.close() 
+		cSocket.close()
+except KeyboardInterrupt:
+	pass
+finally:
+	print "Shutting down..."
+	log("Server shutted down.")
+	logFile.close()
