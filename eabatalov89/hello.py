16c74c9a15e6b169bd1ade47272ca8d38cf310ca
eabatalov89 <eabatalov89@gmail.com>
2011-06-26 23:27:14 +0400

    my server

---
+++ eabatalov89/hello.py
@@ -0,0 +1,22 @@
+# /usr/bin/python2.6
+# -*- coding: utf8 -*-
+print "Hello world"
+print "and some HTML here:"
+print  "<h1>Выберите работу:</h1>"
+print \
+"""
+<table>
+   <tr>
+      <th>Должность</th>
+      <th>Зарплата</th>
+   </tr>
+   <tr>
+      <td>Повар 100 р.</td>
+      <td></td>
+   </tr>
+   <tr>
+      <td>Поцик</td>
+      <td>1000 р.</td>
+   </tr>
+</table>
+"""
